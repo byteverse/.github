4dc525dabf34f59a74788a85331725b96c69b253
Brian McKeon <135748266+brianjosephmckeon@users.noreply.github.com>
2024-01-29 19:48:29 -0500

    Fixes and refactoring.

--- .github/check-release.sh
+++ .github/check-release.sh
@@ -24,70 +24,71 @@ MAINTAINER_EMAIL="$2"
 
 echo "Running pre-release checks for version ${RELEASE_VERSION}"
 
-# Ensure version number in cabal file matches release version.
-CABAL_VERSION=$(awk '$1=="version:"{print $2}' "${CABAL_FILE}")
+# Cabal: Ensure version number matches release version.
+CABAL_VERSION_FIELD=$(awk '$1=="version:"{print $2}' "${CABAL_FILE}")
 
-if [ -z ${CABAL_VERSION} ];
+if [ -z ${CABAL_VERSION_FIELD} ];
 then
   die "Couldn't find version number in ${CABAL_FILE}"
 fi
 
 CABAL_VERSION_VALID=true
-if [ ${CABAL_VERSION} != ${RELEASE_VERSION} ];
+if [ ${CABAL_VERSION_FIELD} != ${RELEASE_VERSION} ];
 then
   CABAL_VERSION_VALID=false
   echo "You need to update the version number in ${CABAL_FILE} to ${RELEASE_VERSION}"
 fi
 
-# Ensure git url is correct in cabal file.
-CABAL_GIT_URL=$(awk '$1=="bug-reports:"{print $2}' "${CABAL_FILE}")
+# Cabal: Ensure bug-reports field exists and is correct.
+CABAL_BUG_REPORTS_FIELD=$(awk '$1=="bug-reports:"{print $2}' "${CABAL_FILE}")
 
-if [ -z ${CABAL_GIT_URL} ];
+if [ -z ${CABAL_BUG_REPORTS_FIELD} ];
 then
   die "Couldn't find bug-reports field in ${CABAL_FILE}"
 fi
 
 REPO_GIT_URL=$(git config --get remote.origin.url)
 EXPECTED_GIT_URL="${REPO_GIT_URL%.git}"
+EXPECTED_CABAL_BUG_REPORTS="${EXPECTED_GIT_URL}/issues"
 
-CABAL_GIT_URL_VALID=true
-if [ ${CABAL_GIT_URL} != ${EXPECTED_GIT_URL} ];
+CABAL_BUG_REPORTS_VALID=true
+if [ ${CABAL_BUG_REPORTS_FIELD} != ${EXPECTED_CABAL_BUG_REPORTS} ];
 then
-  CABAL_GIT_URL_VALID=false
-  echo "You need to change the bug-reports field in ${CABAL_FILE} to ${EXPECTED_GIT_URL}"
+  CABAL_BUG_REPORTS_VALID=false
+  echo "You need to change the bug-reports field in ${CABAL_FILE} to ${EXPECTED_CABAL_BUG_REPORTS}"
 fi
 
-# Ensure homepage field exists in cabal file and is correct.
-CABAL_HOMEPAGE=$(awk '$1=="homepage:"{print $2}' "${CABAL_FILE}")
+# Cabal: Ensure homepage field exists and is correct.
+CABAL_HOMEPAGE_FIELD=$(awk '$1=="homepage:"{print $2}' "${CABAL_FILE}")
 
-if [ -z ${CABAL_HOMEPAGE} ];
+if [ -z ${CABAL_HOMEPAGE_FIELD} ];
 then
   die "Couldn't find homepage field in ${CABAL_FILE}"
 fi
 
 CABAL_HOMEPAGE_VALID=true
-if [ ${CABAL_HOMEPAGE} != ${EXPECTED_GIT_URL} ];
+if [ ${CABAL_HOMEPAGE_FIELD} != ${EXPECTED_GIT_URL} ];
 then
   CABAL_HOMEPAGE_VALID=false
   echo "You need to change the homepage field in ${CABAL_FILE} to ${EXPECTED_GIT_URL}"
 fi
 
-# Ensure maintainer email address is correct in cabal file.
-CABAL_EMAIL=$(awk '$1=="maintainer:"{print $2}' "${CABAL_FILE}")
+# Cabal: Ensure maintainer exists and is correct.
+CABAL_MAINTAINER_FIELD=$(awk '$1=="maintainer:"{print $2}' "${CABAL_FILE}")
 
-if [ -z ${CABAL_EMAIL} ];
+if [ -z ${CABAL_MAINTAINER_FIELD} ];
 then
   die "Couldn't find maintainer field in ${CABAL_FILE}"
 fi
 
-CABAL_EMAIL_VALID=true
-if [ ${CABAL_EMAIL} != ${MAINTAINER_EMAIL} ];
+CABAL_MAINTAINER_VALID=true
+if [ ${CABAL_MAINTAINER_FIELD} != ${MAINTAINER_EMAIL} ];
 then
-  CABAL_EMAIL_VALID=false
+  CABAL_MAINTAINER_VALID=false
   echo "You need to change the maintainer field in ${CABAL_FILE} to ${MAINTAINER_EMAIL}"
 fi
 
-# Ensure source-repository entry exists and is correct.
+# Cabal: Ensure source-repository entry exists and is correct.
 EXPECTED_SOURCE_REPOSITORY_LOCATION="${EXPECTED_GIT_URL#https:\/\/}"
 CABAL_SOURCE_REPOSITORY=$(awk '$1=="source-repository"{print $1}' "${CABAL_FILE}")
 
@@ -100,13 +101,13 @@ FULL_GIT_URL="git://${EXPECTED_SOURCE_REPOSITORY_LOCATION}.git"
 CABAL_SOURCE_REPOSITORY_LOCATION=$(awk '$1=="location:"{print $2}' "${CABAL_FILE}")
 
 CABAL_SOURCE_REPOSITORY_VALID=true
-if [ ${CABAL_SOURCE_REPOSITORY_LOCATION} != "${FULL_GIT_URL}" ];
+if [ -z ${CABAL_SOURCE_REPOSITORY_LOCATION} ] || [ ${CABAL_SOURCE_REPOSITORY_LOCATION} != "${FULL_GIT_URL}" ];
 then
   CABAL_SOURCE_REPOSITORY_VALID=false
   echo "You need to change the source-repository's location in ${CABAL_FILE} to ${FULL_GIT_URL}"
 fi
 
-# Ensure changelog contains an entry for this release version.
+# Changelog: Ensure it contains an entry for this release version.
 CHANGELOG_VERSION=$(grep -Eo "##\s+${RELEASE_VERSION}" "${CHANGELOG_FILE}" || true)
 
 CHANGELOG_VERSION_VALID=true
@@ -118,9 +119,9 @@ fi
 
 CHECKS=(
   "$CABAL_VERSION_VALID"
-  "$CABAL_GIT_URL_VALID"
+  "$CABAL_BUG_REPORTS_VALID"
   "$CABAL_HOMEPAGE_VALID"
-  "$CABAL_EMAIL_VALID"
+  "$CABAL_MAINTAINER_VALID"
   "$CABAL_SOURCE_REPOSITORY_VALID"
   "$CHANGELOG_VERSION_VALID"
 )
